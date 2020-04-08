daa2d2709b6ce51b9a2125fbcf355d10f6881deb
Ethan <me@ethan.link>
2020-04-08 22:45:15 +1000

    Add loading="lazy" to all images in Markdown posts!!

---
+++ _plugins/markdown.rb
@@ -0,0 +1,24 @@
+class Kramdown::Converter::Html
+  def convert_img(el, _indent)
+    "<img#{html_attributes(el.attr)} loading=\"lazy\" />"
+  end
+end
+
+class Jekyll::Converters::Markdown::MyCustomProcessor
+  def initialize(config)
+    require 'kramdown'
+    @config = config
+  rescue LoadError
+    STDERR.puts 'You are missing a library required for Markdown. Please run:'
+    STDERR.puts '  $ [sudo] gem install kramdown'
+    raise FatalException.new("Missing dependency: kramdown")
+  end
+
+  def convert(content)
+    # ::FunkyMarkdown.new(content).convert
+    Kramdown::Document.new(content, {
+      input: 'GFM',
+      syntax_highlighter: 'rouge'
+    }).to_html
+  end
+end
