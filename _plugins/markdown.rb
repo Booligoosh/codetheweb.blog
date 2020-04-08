dfd2a2c966bf50994b940a0c360f2149c298ee7a
Ethan <me@ethan.link>
2020-04-08 23:05:54 +1000

    Added comment to markdown.rb

--- _plugins/markdown.rb
+++ _plugins/markdown.rb
@@ -1,4 +1,6 @@
 class Kramdown::Converter::Html
+  # Overrides this function:
+  # https://kramdown.gettalong.org/rdoc/Kramdown/Converter/Html.html#method-i-convert_img
   def convert_img(el, _indent)
     "<img#{html_attributes(el.attr)} loading=\"lazy\" />"
   end
