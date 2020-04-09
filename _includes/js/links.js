4e1ab800c0dbab65b57f92d70e4105ee79d907d2
Ethan <me@ethan.link>
2020-04-09 15:49:42 +1000

    Fixed links.js I think

--- _includes/js/links.js
+++ _includes/js/links.js
@@ -1,12 +1,12 @@
 /* make all links open in new tab */
-var markdownBody = document.querySelector('.markdown-body')
+var markdownBody = document.querySelector('.markdown-body');
 if (markdownBody) {
-    var links = markdownBody.getElementsByTagName('a')
+    var links = markdownBody.getElementsByTagName('a');
     for (var i = 0; i < links.length; i++){
-        var parser = document.createElement('a')
-        parser.href = links[i].href
+        var parser = document.createElement('a');
+        parser.href = links[i].href;
         if ((parser.origin != window.location.origin || (parser.origin == window.location.origin && parser.pathname == "/out") /*|| window.location.pathname.includes('/learn/')*/ ) && links[i].target === ""){
-            links[i].target="_blank"
+            links[i].target = "_blank";
         }
     }
 }
