681487fbaa1804155fd0b1a6ad698d15b61063b3
Ethan <me@ethan.link>
2020-04-09 15:52:06 +1000

    Load Disqus immediately if page URL has #comment-xyz

--- _includes/js/disqus.js
+++ _includes/js/disqus.js
@@ -1,29 +1,37 @@
 /* https://usefulangle.com/post/251/disqus-comments-improve-page-load-speed */
-var disqus_observer = new IntersectionObserver(
-  function(entries) {
-    /*
-    comments section reached
-    start loading Disqus now
-    */
-    if (entries[0].isIntersecting) {
-      /*
-      var disqus_config = function () {
-        this.page.url = PAGE_URL;
-        this.page.identifier = PAGE_IDENTIFIER;
-      };
-      */
-      (function() {
-        var d = document,
-          s = d.createElement("script");
-        s.src = "{{ site.disqus_url }}";
-        s.setAttribute("data-timestamp", +new Date());
-        (d.head || d.body).appendChild(s);
-      })();
 
-      /* once executed, stop observing */
-      disqus_observer.disconnect();
-    }
-  },
-  { threshold: [0] }
-);
-disqus_observer.observe(document.querySelector("#disqus_thread"));
+/*
+var disqus_config = function () {
+  this.page.url = PAGE_URL;
+  this.page.identifier = PAGE_IDENTIFIER;
+};
+*/
+
+if (window.location.hash.startsWith('#comment')) {
+  /* Make sure that direct links to comments still work */
+  loadDisqus();
+} else {
+  var disqus_observer = new IntersectionObserver(
+    function (entries) {
+      /*
+      comments section reached
+      start loading Disqus now
+      */
+      if (entries[0].isIntersecting) {
+        loadDisqus();
+        /* once executed, stop observing */
+        disqus_observer.disconnect();
+      }
+    },
+    { threshold: [0] }
+  );
+  disqus_observer.observe(document.querySelector("#disqus_thread"));
+}
+
+function loadDisqus() {
+  var d = document,
+    s = d.createElement("script");
+  s.src = "{{ site.disqus_url }}";
+  s.setAttribute("data-timestamp", +new Date());
+  (d.head || d.body).appendChild(s);
+}
