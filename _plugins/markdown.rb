47a0ae2f3ac5acecba15c2c931872749d7fdfd6b
Ethan <me@ethan.link>
2020-04-10 13:18:37 +1000

    Add target="_blank" to links during compile with Ruby instead of using JS!

--- _plugins/markdown.rb
+++ _plugins/markdown.rb
@@ -4,6 +4,19 @@ class Kramdown::Converter::Html
   def convert_img(el, _indent)
     "<img#{html_attributes(el.attr)} loading=\"lazy\" />"
   end
+  # Overrides this function:
+  # https://kramdown.gettalong.org/rdoc/Kramdown/Converter/Html.html#method-i-convert_a
+  def convert_a(el, indent)
+    href = el.attr['href']
+    if href
+      url = URI(href)
+      host = url.host
+      if host and host != 'codetheweb.blog'
+        el.attr['target'] = '_blank'
+      end
+    end
+    format_as_span_html(el.type, el.attr, inner(el, indent))
+  end
 end
 
 class Jekyll::Converters::Markdown::MyCustomProcessor
