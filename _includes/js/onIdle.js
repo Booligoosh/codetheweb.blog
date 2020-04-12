fc5a1c35e7dde6c2d37e63097d1a4a3417b6c8fe
Ethan <me@ethan.link>
2020-04-12 12:55:47 +1000

    Test removing Crisp

--- _includes/js/onIdle.js
+++ _includes/js/onIdle.js
@@ -18,17 +18,17 @@ window.onload = function() {
   }
 
   function loadCrisp() {
-    console.log("LOADING CRISP");
-    window.$crisp = [];
-    window.CRISP_WEBSITE_ID = "c2a98cd5-6c04-4a59-8ff6-ab04f67a3e7d";
-    (function() {
-      d = document;
-      s = d.createElement("script");
-      s.src = "https://client.crisp.chat/l.js";
-      s.async = 1;
-      s.defer = 1;
-      d.getElementsByTagName("head")[0].appendChild(s);
-    })();
+    // console.log("LOADING CRISP");
+    // window.$crisp = [];
+    // window.CRISP_WEBSITE_ID = "c2a98cd5-6c04-4a59-8ff6-ab04f67a3e7d";
+    // (function() {
+    //   d = document;
+    //   s = d.createElement("script");
+    //   s.src = "https://client.crisp.chat/l.js";
+    //   s.async = 1;
+    //   s.defer = 1;
+    //   d.getElementsByTagName("head")[0].appendChild(s);
+    // })();
   }
 
   function loadGoogleAnalytics() {
