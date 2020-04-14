ec2672b73200d0d2b606f48499038069707907c3
Ethan <me@ethan.link>
2020-04-14 13:49:55 +1000

    Optimized Codefund!

--- _includes/js/codefund.js
+++ _includes/js/codefund.js
@@ -7,15 +7,22 @@ if (window.requestIdleCallback) {
   loadCodeFund();
 }
 function loadCodeFund() {
-  console.log("LOADING CODEFUND");
+  /*console.log("LOADING CODEFUND");
   var d = document,
     s = d.createElement("script");
   s.src = "https://app.codefund.io/properties/608/funder.js";
   s.async = 1;
   s.defer = 1;
-  (d.head || d.body).appendChild(s);
+  (d.head || d.body).appendChild(s);*/
 
-  /* Preconnect to Codefund's various domains */
+  /* Preconnect to Codefund's various domains */ /*
   document.head.innerHTML +=
     '<link rel="preconnect" href="https://cdn2.codefund.app/" crossorigin><link rel="preconnect" href="https://cdn2.codefund.io/" crossorigin>';
+  */
+
+  /* Based on https://codefund.docs.apiary.io/#reference/0/advertisement-html
+     This method required less JS and fewer domain connections */
+  fetch("https://api.codefund.app/properties/608/funder.html")
+    .then(r => r.text())
+    .then(html => (document.getElementById("codefund").innerHTML = html));
 }
