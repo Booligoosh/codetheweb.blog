eca03e9f2fab7c3cb26f965bff770e797affc810
Ethan <me@ethan.link>
2020-04-17 19:17:18 +1000

    Moved Codefund code into onIdle.js

--- _includes/js/onIdle.js
+++ _includes/js/onIdle.js
@@ -11,6 +11,9 @@
   }
 
   function onIdle() {
+    {% if page.layout == 'post' %}
+    loadCodeFund();
+    {% endif %}
     loadCrisp();
     {% if jekyll.environment == "production" %}
     loadGoogleAnalytics();
@@ -34,6 +37,7 @@
   }
 
   function loadGoogleAnalytics() {
+    console.log('LOADING GOOGLE ANALYTICS');
     /*
     window.dataLayer = window.dataLayer || [];
     window.gtag = function() {
@@ -60,5 +64,40 @@
     galite('send', 'pageview');
 
     document.head.innerHTML += '<link rel="preconnect" href="https://www.google-analytics.com/" crossorigin>';
+  }
+
+  function loadCodeFund() {
+    console.log("LOADING CODEFUND");
+    /*console.log("LOADING CODEFUND");
+    var d = document,
+      s = d.createElement("script");
+    s.src = "https://app.codefund.io/properties/608/funder.js";
+    s.async = 1;
+    s.defer = 1;
+    (d.head || d.body).appendChild(s);*/
+
+    /* Preconnect to Codefund's various domains */ /*
+    document.head.innerHTML +=
+      '<link rel="preconnect" href="https://cdn2.codefund.app/" crossorigin><link rel="preconnect" href="https://cdn2.codefund.io/" crossorigin>';
+    */
+
+    /* Based on https://codefund.docs.apiary.io/#reference/0/advertisement-html
+      This method required less JS and fewer domain connections */
+    fetch("https://api.codefund.app/properties/608/funder.html?$third-party")
+      .then(r => r.text())
+      .then(
+        html =>
+          (document.getElementById("codefund").innerHTML = html
+            /* Remove stylesheet link to save network usage */
+            .replace(/<link .*?rel="stylesheet".*?>/g, ""))
+      )
+      .catch(
+        () =>
+          (document.getElementById("codefund").innerHTML =
+            '<div class="codefund-placeholder" style="text-align: left">Please consider disabling your ad blocker to support this site.<br><br>This rectangle is the only place on the page where ads are shown.</div>')
+      );
+
+    document.head.innerHTML +=
+      '<link rel="preconnect" href="https://cdn2.codefund.io/" crossorigin>';
   }
 /*};*/
